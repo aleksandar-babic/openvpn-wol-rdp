a8eea7047a18c892b42072c4baef025afde05c24
aleksandarbabic <alekski64@icloud.com>
2016-12-08 19:12:36 +0100

    Added check mac address function.

--- OpenVPN-WOL-RDP_script/OpenVPN-WOL-RDP_script/Program.cs
+++ OpenVPN-WOL-RDP_script/OpenVPN-WOL-RDP_script/Program.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace OpenVPN_WOL_RDP_script
 {
     class Program
     {
+        public static string checkMacFormat(string macAddr) {
+
+            macAddr = macAddr.Replace(" ", ":").Replace("-", ":");
+
+            Regex r = new Regex("^(?:[0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}|(?:[0-9a-fA-F]{2}-){5}[0-9a-fA-F]{2}|(?:[0-9a-fA-F]{2}){5}[0-9a-fA-F]{2}$");
+
+
+            if (r.IsMatch(macAddr))
+            {
+                Console.WriteLine("Valid Mac address");
+                return macAddr;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Mac");
+                return null;
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello RDP!");
+            string checkAddr = "88-AE-1D-41-87-58";
+            
         }
     }
 }
