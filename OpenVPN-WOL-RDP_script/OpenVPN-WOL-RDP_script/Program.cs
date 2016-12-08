309f580a8853ddfda509b15014ea30d2ea112060
aleksandarbabic <alekski64@icloud.com>
2016-12-08 19:32:18 +0100

    Fixed MAC check and added subnet broadcast IP check.

--- OpenVPN-WOL-RDP_script/OpenVPN-WOL-RDP_script/Program.cs
+++ OpenVPN-WOL-RDP_script/OpenVPN-WOL-RDP_script/Program.cs
@@ -14,24 +14,54 @@ namespace OpenVPN_WOL_RDP_script
             macAddr = macAddr.Replace(" ", ":").Replace("-", ":");
 
             Regex r = new Regex("^(?:[0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}|(?:[0-9a-fA-F]{2}-){5}[0-9a-fA-F]{2}|(?:[0-9a-fA-F]{2}){5}[0-9a-fA-F]{2}$");
+            bool isValid = false;
 
-
-            if (r.IsMatch(macAddr))
+            while (!isValid)
             {
-                Console.WriteLine("Valid Mac address");
-                return macAddr;
+                if (r.IsMatch(macAddr))
+                {
+                    Console.WriteLine("Valid Mac address!");
+                    return macAddr;
+                }
+                else
+                {
+                    Console.Write("Invalid Mac, try again. Mac Address : ");
+                    macAddr = Console.ReadLine();
+                    macAddr = macAddr.Replace(" ", ":").Replace("-", ":");
+                }
             }
-            else
-            {
-                Console.WriteLine("Invalid Mac");
-                return null;
-            }
+            return null;
+        }
 
+        public static Boolean sendWOL(string macAddr, string subnetBroadcast) {
+            if (checkMacFormat(macAddr) != null)
+            {
+                var match = Regex.Match(subnetBroadcast, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                bool isValid = false;
+                while (!isValid)
+                {
+                    if (match.Success)
+                    {
+                        Console.WriteLine("Good IP!");
+                        isValid = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid IP address for subnet broadcast, try again : ");
+                        subnetBroadcast = Console.ReadLine();
+                        match = Regex.Match(subnetBroadcast, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                    }
+                }
+                Console.WriteLine(macAddr + "   " + subnetBroadcast);
+            }
+            return false;
         }
         static void Main(string[] args)
         {
-            string checkAddr = "88-AE-1D-41-87-58";
-            
+            string checkAddr = "88-AE-1D-41D-87-58";
+            //Console.WriteLine(checkMacFormat(checkAddr));
+            sendWOL(checkAddr,"192.168.1.2");
+
         }
     }
 }
