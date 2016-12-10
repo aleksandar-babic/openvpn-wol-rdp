0ba0fb8a9717ee5108a7417fc5d9cd6ab460e832
aleksandarbabic <alekski64@icloud.com>
2016-12-10 14:09:55 +0100

    Added multithreaded network discovery.

--- OpenVPN-WOL-RDP_script/OpenVPN-WOL-RDP_script/Program.cs
+++ OpenVPN-WOL-RDP_script/OpenVPN-WOL-RDP_script/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace OpenVPN_WOL_RDP_script
 {
@@ -30,29 +31,36 @@ namespace OpenVPN_WOL_RDP_script
         }
 
         public static Boolean sendWOL(string macAddr, string subnetBroadcast) {
-            if (checkMacFormat(macAddr) != null)
+            try
             {
-                var match = Regex.Match(subnetBroadcast, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
-                bool isValid = false;
-                while (!isValid)
+                if (checkMacFormat(macAddr) != null)
                 {
-                    if (match.Success)
+                    var match = Regex.Match(subnetBroadcast, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                    bool isValid = false;
+                    while (!isValid)
                     {
-                        Console.WriteLine("Everything looks good!\n\nMAC:{0}\nSubnet Broadcast IP :{1}\n\n",macAddr,subnetBroadcast);
-                        Console.WriteLine("Sending WOL Packet..");
-                        string strWOLCall;
-                        strWOLCall = "/C mc-wol.exe "+ macAddr +" /a " + subnetBroadcast;
-                        System.Diagnostics.Process.Start("CMD.exe", strWOLCall);
-                        Console.WriteLine("WOL Packet sent!");
-                        isValid = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid IP address for subnet broadcast, try again : ");
-                        subnetBroadcast = Console.ReadLine();
-                        match = Regex.Match(subnetBroadcast, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                        if (match.Success)
+                        {
+                            Console.WriteLine("Everything looks good!\nMAC:{0}\nSubnet Broadcast IP :{1}\n\n", macAddr, subnetBroadcast);
+                            Console.WriteLine("Sending WOL Packet..");
+                            string strWOLCall;
+                            strWOLCall = "/C mc-wol.exe " + macAddr + " /a " + subnetBroadcast;
+                            System.Diagnostics.Process.Start("CMD.exe", strWOLCall);
+                            Thread.Sleep(2000);
+                            Console.WriteLine("WOL Packet sent!");
+                            isValid = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid IP address for subnet broadcast, try again : ");
+                            subnetBroadcast = Console.ReadLine();
+                            match = Regex.Match(subnetBroadcast, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                        }
                     }
                 }
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Could not send WOL packet. Error : {0}",ex);
             }
             return false;
         }
@@ -60,35 +68,70 @@ namespace OpenVPN_WOL_RDP_script
         public static void pingPartNetwork(string range,string subnetBroadcast) {
             var match = Regex.Match(subnetBroadcast, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
             bool isValid = false;
-            while (!isValid)
+            try
             {
-                if (match.Success)
+                while (!isValid)
                 {
-                    string[] strTmpRange = range.Split('-');
-                    string[] strTmp = subnetBroadcast.Split('.');
-                    string strFinal = strTmp[0] + "." + strTmp[1] + "." + strTmp[2] + ".";
-                    Console.WriteLine("Range = {0} - {1} ; Subnet = {2}", strTmpRange[0], strTmpRange[1], strFinal);
-                    for (int i = Convert.ToInt32(strTmpRange[0]); i < Convert.ToInt32(strTmpRange[1]); i++)
+                    if (match.Success)
                     {
-                        string strPingCall;
-                        strPingCall = "/C ping " + strFinal + i + " -n 1";
-                        System.Diagnostics.Process.Start("CMD.exe", strPingCall);
+                        string[] strTmpRange = range.Split('-');
+                        string[] strTmp = subnetBroadcast.Split('.');
+                        string strFinal = strTmp[0] + "." + strTmp[1] + "." + strTmp[2] + ".";
+                        //Console.WriteLine("Range = {0} - {1} ; Subnet = {2}", strTmpRange[0], strTmpRange[1], strFinal);
+                        for (int i = Convert.ToInt32(strTmpRange[0]); i < Convert.ToInt32(strTmpRange[1]); i++)
+                        {
+                            string strPingCall;
+                            strPingCall = "/C ping " + strFinal + i + " -n 1";
+                            //System.Diagnostics.Process.Start("CMD.exe", strPingCall);
+                            System.Diagnostics.Process process = new System.Diagnostics.Process();
+                            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                            startInfo.FileName = "cmd.exe";
+                            startInfo.Arguments = strPingCall;
+                            process.StartInfo = startInfo;
+                            process.Start();
+
+                        }
+                        isValid = true;
                     }
-                    isValid = true;
+                    else
+                    {
+                        Console.WriteLine("Invalid IP address format for network discovery.");
+                        break;
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Invalid IP address format for network discovery.");
-                    break;
-                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not discover all network devices. Error : {0}", ex);
             }
         }
         static void Main(string[] args)
         {
             string checkAddr = "88:AE:1D:41:87:58";
-            //Console.WriteLine(checkMacFormat(checkAddr));
-            sendWOL(checkAddr,"10.10.0.255");
-            pingPartNetwork("100-200", "10.10.0.255");
+            string broadcastIP = "10.10.0.255";
+            Thread worker1 = new Thread(() => pingPartNetwork("0-100", broadcastIP));
+            Thread worker2 = new Thread(() => pingPartNetwork("101-200", broadcastIP));
+            Thread worker3 = new Thread(() => pingPartNetwork("201-254", broadcastIP));
+            Console.WriteLine("Starting network discovery..");
+            worker1.Start();
+            worker2.Start();
+            worker3.Start();
+            worker1.Join();
+            worker2.Join();
+            worker3.Join();
+            Console.WriteLine("Network discovery done!\n");
+            sendWOL(checkAddr, "10.10.0.255");
+
+
+
+
+
+
+
+
+
+
             Console.Read();
         }
     }
