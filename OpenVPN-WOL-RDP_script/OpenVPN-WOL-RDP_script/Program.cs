b548b3f220c1b46178bf225c8e483dd12ff6a0b4
aleksandarbabic <alekski64@icloud.com>
2016-12-11 17:47:50 +0100

    !IMPORTANT : Changing way script works, WOL is moved to external PHP script, this script is only calling it.

--- OpenVPN-WOL-RDP_script/OpenVPN-WOL-RDP_script/Program.cs
+++ OpenVPN-WOL-RDP_script/OpenVPN-WOL-RDP_script/Program.cs
@@ -9,12 +9,14 @@ using System.Diagnostics;
 using System.IO;
 using System.Net.NetworkInformation;
 using System.ServiceProcess;
+using System.Net;
 
 namespace OpenVPN_WOL_RDP_script
 {
     class Program
     {
-        public static string checkMacFormat(string macAddr) {
+        public static string checkMacFormat(string macAddr)
+        {
 
             macAddr = macAddr.Replace(" ", ":").Replace("-", ":");
 
@@ -34,146 +36,50 @@ namespace OpenVPN_WOL_RDP_script
             return null;
         }
 
-        public static Boolean sendWOL(string macAddr, string subnetBroadcast) {
-            try
+        public static string checkIPandCreateBroadcast(string ip)
+        {
+            Match match = Regex.Match(ip, @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+            if (match.Success)
             {
-                if (checkMacFormat(macAddr) != null)
+                string[] tmpip = ip.Split('.');
+                ip = tmpip[0] + '.' + tmpip[1] + '.' + tmpip[2] + ".255";
+                return ip;
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+
+        public static bool isReachable(string ipAddr)
+        {
+            Console.WriteLine("Waiting for {0} to become reachable.", ipAddr);
+            Ping p = new Ping();
+            PingReply r;
+            while (true)
+            {
+                if (ipAddr != null)
                 {
-                    var match = Regex.Match(subnetBroadcast, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
-                    string args = String.Format("{0} /a {1}", macAddr, subnetBroadcast);
-                    macAddr = checkMacFormat(macAddr);
-                    bool isValid = false;
-                    while (!isValid)
+                    r = p.Send(ipAddr);
+                    if (r.Status == IPStatus.Success)
                     {
-                        if (match.Success)
-                        {
-                            Console.WriteLine("Everything looks good!\nMAC:{0}\nSubnet Broadcast IP :{1}\n\n", macAddr, subnetBroadcast);
-                            Console.WriteLine("Sending WOL Packet..");
-                            string strWOLCall;
-                            strWOLCall = macAddr + " /a " + subnetBroadcast;
-                            Process.Start("mc-wol.exe", strWOLCall);
-                            Thread.Sleep(2000);
-                            Console.WriteLine("WOL Packet sent!\n\n");
-                            isValid = true;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid IP address for subnet broadcast, try again : ");
-                            subnetBroadcast = Console.ReadLine();
-                            match = Regex.Match(subnetBroadcast, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
-                        }
+                        Console.WriteLine("{0} is reachable!", ipAddr);
+                        return true;
                     }
                 }
-            }
-            catch (Exception ex) {
-                Console.WriteLine("Could not send WOL packet. Error : {0}",ex);
+                else break;
             }
             return false;
         }
 
-        public static void pingPartNetwork(string range,string subnetBroadcast) {
-            var match = Regex.Match(subnetBroadcast, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
-            bool isValid = false;
-            try
-            {
-                while (!isValid)
-                {
-                    if (match.Success)
-                    {
-                        string[] strTmpRange = range.Split('-');
-                        string[] strTmp = subnetBroadcast.Split('.');
-                        string strFinal = strTmp[0] + "." + strTmp[1] + "." + strTmp[2] + ".";
-                        for (int i = Convert.ToInt32(strTmpRange[0]); i < Convert.ToInt32(strTmpRange[1]); i++)
-                        {
-                            string strPingCall;
-                            strPingCall = "/C ping " + strFinal + i + " -n 1";
-                            Process process = new Process();
-                            ProcessStartInfo startInfo = new ProcessStartInfo();
-                            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                            startInfo.FileName = "cmd.exe";
-                            startInfo.Arguments = strPingCall;
-                            process.StartInfo = startInfo;
-                            process.Start();
-
-                        }
-                        isValid = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid IP address format for network discovery.");
-                        break;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Could not discover all network devices. Error : {0}", ex);
-            }
-        }
-
-        public string getMacByIp(string ip)
+        public static void startRDP(string ip)
         {
-            var macIpPairs = GetAllMacAddressesAndIppairs();
-            int index = macIpPairs.FindIndex(x => x.IpAddress == ip);
-            if (index >= 0)
-            {
-                return macIpPairs[index].MacAddress.ToUpper();
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public struct MacIpPair
-        {
-            public string MacAddress;
-            public string IpAddress;
-        }
-
-        public static List<MacIpPair> GetAllMacAddressesAndIppairs()
-        {
-            List<MacIpPair> mip = new List<MacIpPair>();
-            Process pProcess = new Process();
-            pProcess.StartInfo.FileName = "arp";
-            pProcess.StartInfo.Arguments = "-a ";
-            pProcess.StartInfo.UseShellExecute = false;
-            pProcess.StartInfo.RedirectStandardOutput = true;
-            pProcess.StartInfo.CreateNoWindow = true;
-            pProcess.Start();
-            string cmdOutput = pProcess.StandardOutput.ReadToEnd();
-            string pattern = @"(?<ip>([0-9]{1,3}\.?){4})\s*(?<mac>([a-f0-9]{2}-?){6})";
-
-            foreach (Match m in Regex.Matches(cmdOutput, pattern, RegexOptions.IgnoreCase))
-            {
-                mip.Add(new MacIpPair()
-                {
-                    MacAddress = m.Groups["mac"].Value,
-                    IpAddress = m.Groups["ip"].Value
-                });
-            }
-
-            return mip;
-        }
-
-        public static string getIP(string macAddr) {
-            macAddr = macAddr.Replace(" ", "-").Replace(":", "-").ToLower();
-            List<MacIpPair> tmp = GetAllMacAddressesAndIppairs();
-            foreach (var v in tmp) {
-                if (macAddr == v.MacAddress) {
-                    Console.WriteLine("Found IP : {0} for requested MAC : {1}", v.IpAddress, macAddr);
-                    return v.IpAddress;
-                }
-            }
-            return "";
-        }
-
-        public static void startRDP(string macAddr) {
-            if (isReachable(getIP(macAddr)))
+            if (isReachable(ip))
             {
                 Console.WriteLine("\n\nGiving machine 20seconds to finish booting after becoming reachable.");
                 Thread.Sleep(20000);
-                string strConnectIP = getIP(macAddr);
+                string strConnectIP = ip;
                 string strRDPCall;
                 strRDPCall = "/v " + strConnectIP;
                 Process process = new Process();
@@ -185,26 +91,45 @@ namespace OpenVPN_WOL_RDP_script
                 process.Start();
                 Console.WriteLine("\n\nStarted RDP session for IP : {0}", strConnectIP);
             }
-            else {
-                Console.WriteLine("Can't start RDP, machine({0}) is not reachable", getIP(macAddr));
-            }
-
-        }
-        public static bool isReachable(string ipAddr) {
-            Console.WriteLine("Waiting for {0} to become reachable.", ipAddr);
-            Ping p = new Ping();
-            PingReply r;
-            while (true)
+            else
             {
-                r = p.Send(ipAddr);
-                if (r.Status == IPStatus.Success) {
-                    Console.WriteLine("{0} is reachable!", ipAddr);
-                    return true;
-                }
+                Console.WriteLine("Can't start RDP, machine({0}) is not reachable", ip);
             }
+
         }
 
-        public static string checkOpenVPNService() {
+        public static bool sendWOL(string macAddr,string ip) {
+            if (((macAddr = checkMacFormat(macAddr)) != null) && (checkIPandCreateBroadcast(ip) != "")) {
+                string html = string.Empty;
+                string url = @"http://10.10.0.14/sendwol.php?mac=" + macAddr + "&ip=" + checkIPandCreateBroadcast(ip);
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    request.AutomaticDecompression = DecompressionMethods.GZip;
+
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        html = reader.ReadToEnd();
+                    }
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("Error connecting to WOL server. Full error : {0}",ex);
+                    return false;
+                }
+
+
+                Console.WriteLine("Response from WOL server : {0}",html);
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public static string checkOpenVPNService()
+        {
             ServiceController sc = new ServiceController("OpenVPNService");
 
             switch (sc.Status)
@@ -224,13 +149,15 @@ namespace OpenVPN_WOL_RDP_script
             }
         }
 
-        public static bool connectToOpenVPN() {
+        public static bool connectToOpenVPN()
+        {
             if (checkOpenVPNService() == "Running")
             {
                 Console.WriteLine("OpenVPN service is already running.");
                 return true;
             }
-            else if (checkOpenVPNService() == "Stopped") {
+            else if (checkOpenVPNService() == "Stopped")
+            {
                 Console.WriteLine("OpenVPN service is stopped.");
                 Console.WriteLine("Trying to start OpenVPN service.");
                 ServiceController sc = new ServiceController("OpenVPNService");
@@ -245,8 +172,9 @@ namespace OpenVPN_WOL_RDP_script
                         return true;
                     }
                 }
-                catch (Exception ex) {
-                    Console.WriteLine("Error starting OpenVPN service. Error : {0}",ex);
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error starting OpenVPN service. Error : {0}", ex);
                 }
             }
             return false;
@@ -255,32 +183,23 @@ namespace OpenVPN_WOL_RDP_script
         static void Main(string[] args)
         {
             string checkAddr = "";
-            string broadcastIP = "";
+            string rdpIP = "";
 
-            if (args.Length == 2)
+           if (args.Length == 2)
             {
                 checkAddr = args[0];
-                broadcastIP = args[1];
+                rdpIP = args[1];
                 if (connectToOpenVPN() != false)
-                {                                                
-                    if (broadcastIP != "" && checkAddr != "")
+                {
+                    if (checkIPandCreateBroadcast(rdpIP) != "" && checkMacFormat(checkAddr) != null)
                     {
-                        Thread worker1 = new Thread(() => pingPartNetwork("0-100", broadcastIP));
-                        Thread worker2 = new Thread(() => pingPartNetwork("101-200", broadcastIP));
-                        Thread worker3 = new Thread(() => pingPartNetwork("201-254", broadcastIP));
-                        Console.WriteLine("Starting network discovery..");
-                        worker1.Start();
-                        worker2.Start();
-                        worker3.Start();
-                        worker1.Join();
-                        worker2.Join();
-                        worker3.Join();
-                        Console.WriteLine("Network discovery done!\n");
-                        sendWOL(checkAddr, broadcastIP);
-                        startRDP(checkAddr);
+                        sendWOL(checkAddr,rdpIP);
+                        startRDP(rdpIP);
+                        Console.WriteLine("\n\n\nIf your RDP is started you can close this window by pressing any key.");
+                        Console.Read();
                     }
-                    Console.WriteLine("\n\n\nIf your RDP is started you can close this window by pressing any key.");
-                    Console.Read();
+                    else
+                        Console.WriteLine("IP or MAC invalid.");
                 }
                 else {
                     Console.WriteLine("Could not start OpenVPN.");
@@ -288,10 +207,11 @@ namespace OpenVPN_WOL_RDP_script
             }
             else
             {
-                Console.WriteLine("OpenVPN - WakeOnLan - RDP script v1.0\n\nUsage : OpenVPN-WOL-RDP_script.exe <MAC Address> <IP Broadcast>\nExample : OpenVPN-WOL-RDP_script.exe 88:AE:1D:41:87:58 10.10.0.255\n\nMAC Address is physical IP address of remote pc adapter(you can get it from ipconfig /all on remote pc)\nIP Broadcast - if remote PC address is for example 10.10.0.24 your broadcast ip will be 10.10.0.255\n\n\nClose this window and try again..");
+                Console.WriteLine("OpenVPN - WakeOnLan - RDP script v1.0\n\nUsage : OpenVPN-WOL-RDP_script.exe <MAC Address> <IP Address>\nExample : OpenVPN-WOL-RDP_script.exe 88:AE:1D:41:87:58 10.10.0.15\n\nMAC Address is physical IP address of remote pc adapter(you can get it from ipconfig /all on remote pc)\nIP Address is IP address of remote PC\n\n\nClose this window and try again..");
                 Console.Read();
                 Environment.Exit(-1);
             }
+
         }
     }
 }
